0b1e2a238a121ad5a23842d739c774377e4c0fe8
Federico Arvat <arvat.federico@gmail.com>
2023-08-07 18:18:32 +0200

    Asserted that SessionGet API return 401 status code on non-authenticated request

--- TransmissionAPIEndToEndTests/SessionGetAPIEndToEndTests.swift
+++ TransmissionAPIEndToEndTests/SessionGetAPIEndToEndTests.swift
@@ -9,13 +9,31 @@ import XCTest
 import Transmission
 
 final class SessionGetAPIEndToEndTests: XCTestCase {
-//
+
+	func test_sessionGetAPI_return401StatusCodeOnNonAuthenticatedRequest() {
+		let client = ephemeralClient()
+		let exp = expectation(description: "Wait for load to complete")
+		
+		let baseURL = URL(string: "http://192.168.178.39:9091")!
+		
+		client.post(APIsEndpoint.post.url(baseURL: baseURL), body: SessionBodies.get.data(using: .utf8)!) { result in
+			switch result {
+			case let .failure(error):
+				XCTFail("Expected success, got \(error) instead")
+			case let .success((_, response)):
+				XCTAssertEqual(response.statusCode, 401, "Expected 401, got \(response.statusCode) instead")
+			}
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 5.0)
+	}
+	
 //	func test_sessionGetAPI_isCorrectlyAuthenticated() {
 //		let client = ephemeralClient()
 //		let exp = expectation(description: "Wait for load to complete")
-//		
+//
 //		let baseURL = URL(string: "http://192.168.178.39:9091")!
-//		
+//
 //		var receivedResult: Result<Session, Error>?
 //		client.post(APIsEndpoint.post.url(baseURL: baseURL), body: SessionBodies.get.data(using: .utf8)!) { result in
 //			receivedResult = result.flatMap { (data, response) in
@@ -35,13 +53,13 @@ final class SessionGetAPIEndToEndTests: XCTestCase {
 //			break
 //		}
 //	}
-//	
+//
 //    func test_sessionGetAPI_decodeCorrectlyOn200HTTPResponse() {
 //		let client = ephemeralClient()
 //		let exp = expectation(description: "Wait for load to complete")
-//		
+//
 //		let baseURL = URL(string: "http://192.168.178.39:9091")!
-//		
+//
 //		var receivedResult: Result<Session, Error>?
 //		client.post(APIsEndpoint.post.url(baseURL: baseURL), body: SessionBodies.get.data(using: .utf8)!) { result in
 //			receivedResult = result.flatMap { (data, response) in
@@ -61,13 +79,13 @@ final class SessionGetAPIEndToEndTests: XCTestCase {
 //			break
 //		}
 //	}
-//	
-//	// MARK: - Helpers
-//	
-//	private func ephemeralClient(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
-//		let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-//		trackForMemoryLeaks(client, file: file, line: line)
-//		return client
-//	}
+
+	// MARK: - Helpers
+
+	private func ephemeralClient(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
+		let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+		trackForMemoryLeaks(client, file: file, line: line)
+		return client
+	}
 
 }
