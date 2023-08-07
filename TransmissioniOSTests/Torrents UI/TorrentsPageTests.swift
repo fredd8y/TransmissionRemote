3ff8cc7cade49c126c643bfc27535f539e0ede5f
Federico Arvat <arvat.federico@gmail.com>
2023-08-07 18:18:33 +0200

    Asserted that torrent list with content is displayed correctly

--- TransmissioniOSTests/Torrents UI/TorrentsPageTests.swift
+++ TransmissioniOSTests/Torrents UI/TorrentsPageTests.swift
@@ -24,6 +24,14 @@ class TorrentsPageTests: XCTestCase {
 		assert(snapshot: sut.snapshot(.iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "LIST_WITH_ERROR_MESSAGE_light_extraExtraExtraLarge")
 	}
 	
+	func test_listWithContent() {
+		let sut = makeSUT(nil, torrents: listWithContent())
+		
+		assert(snapshot: sut.snapshot(.iPhone13(style: .light)), named: "LIST_WITH_CONTENT_light")
+		assert(snapshot: sut.snapshot(.iPhone13(style: .dark)), named: "LIST_WITH_CONTENT_dark")
+		assert(snapshot: sut.snapshot(.iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "LIST_WITH_CONTENT_light_extraExtraExtraLarge")
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(_ error: String?, torrents: [TorrentsPage.TorrentViewModel]) -> TorrentsPage {
@@ -32,5 +40,52 @@ class TorrentsPageTests: XCTestCase {
 	
 	private func emptyList() -> [TorrentsPage.TorrentViewModel] {
 		[]
+	}
+	
+	private func listWithContent() -> [TorrentsPage.TorrentViewModel] {
+		[
+			TorrentsPage.TorrentViewModel(
+				name: anyName,
+				eta: anyEta,
+				completionPercentage: anyPercentage,
+				completionPercentageString: anyPercentageString,
+				downloaded: anyDownloaded,
+				downloadSpeed: anyDownloadSpeed
+			),
+			TorrentsPage.TorrentViewModel(
+				name: anyName,
+				eta: anyEta,
+				completionPercentage: anyPercentage,
+				completionPercentageString: anyPercentageString,
+				downloaded: anyDownloaded,
+				downloadSpeed: anyDownloadSpeed
+			)
+		]
+	}
+	
+	// MARK: - Helpers
+	
+	private var anyName: String {
+		"a name"
+	}
+	
+	private var anyEta: String {
+		"6y 7d 5h 34m 12s"
+	}
+	
+	private var anyPercentage: Double {
+		0.76
+	}
+	
+	private var anyPercentageString: String {
+		"76,75%"
+	}
+	
+	private var anyDownloaded: String {
+		"54MB of 5.6GB"
+	}
+	
+	private var anyDownloadSpeed: String {
+		"567Kb/s"
 	}
 }
