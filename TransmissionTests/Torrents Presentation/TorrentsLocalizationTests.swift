87e67046682682861e881f9835808197e2d0cd58
Federico Arvat <arvat.federico@gmail.com>
2023-08-07 18:18:33 +0200

    Asserted that localised strings have keys and values for all supported localisations

---
+++ TransmissionTests/Torrents Presentation/TorrentsLocalizationTests.swift
@@ -0,0 +1,21 @@
+//
+//  TorrentsLocalizationTests.swift
+//  TransmissionTests
+//
+//  Created by Federico Arvat on 19/07/23.
+//
+
+import XCTest
+import Transmission
+
+final class TorrentsLocalizationTests: XCTestCase {
+	
+	func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+		let table = "Torrents"
+		let bundle = Bundle(for: TorrentsPresenter.self)
+		
+		assertLocalizedKeyAndValuesExist(in: bundle, table)
+	}
+	
+}
+
