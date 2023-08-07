fe280b8c921075ed1414d9d63ccad4b8da50b5a5
Federico Arvat <arvat.federico@gmail.com>
2023-08-07 18:18:34 +0200

    Asserted that server page list with content is displayed correctly

--- Transmission/TransmissioniOSTests/Transmission UI/ServerPageTests.swift
+++ Transmission/TransmissioniOSTests/Transmission UI/ServerPageTests.swift
@@ -19,6 +19,14 @@ class ServerPageTests: XCTestCase {
 		assert(snapshot: sut.snapshot(.iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "SERVER_PAGE_EMPTY_LIST_light_extraExtraExtraLarge")
 	}
 	
+	func test_listWithContent() {
+		let sut = makeSUT(servers: listWithContent())
+		
+		assert(snapshot: sut.snapshot(.iPhone13(style: .light)), named: "SERVER_PAGE_LIST_WITH_CONTENT_light")
+		assert(snapshot: sut.snapshot(.iPhone13(style: .dark)), named: "SERVER_PAGE_LIST_WITH_CONTENT_dark")
+		assert(snapshot: sut.snapshot(.iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "SERVER_PAGE_LIST_WITH_CONTENT_light_extraExtraExtraLarge")
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(servers: [ServerViewModel]) -> ServerPage {
@@ -31,5 +39,20 @@ class ServerPageTests: XCTestCase {
 	
 	private func emptyList() -> [ServerViewModel] {
 		[]
+	}
+	
+	private func listWithContent() -> [ServerViewModel] {
+		[
+			ServerViewModel(
+				title: "Raspberry",
+				url: "http://192.168.1.1:9091",
+				id: UUID()
+			),
+			ServerViewModel(
+				title: "NAS",
+				url: "http://192.168.1.2:9091",
+				id: UUID()
+			)
+		]
 	}
 }
