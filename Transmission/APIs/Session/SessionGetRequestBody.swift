12d3d951394b0559878fd9ee590d53588226f3f9
Federico Arvat <arvat.federico@gmail.com>
2023-08-07 18:18:32 +0200

    Asserted that session get return expected httpBody

---
+++ Transmission/APIs/Session/SessionGetRequestBody.swift
@@ -0,0 +1,12 @@
+//
+//  SessionGetRequestBody.swift
+//  Transmission
+//
+//  Created by Federico Arvat on 13/05/23.
+//
+
+import Foundation
+
+public class SessionGetRequestBody {
+	public static let body = #"{"method": "session-get"}"#
+}
