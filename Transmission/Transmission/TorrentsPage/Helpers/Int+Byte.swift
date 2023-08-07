32e3f5ce91183a6316e623b8c2e3cbe1ea9eee86
Federico Arvat <arvat.federico@gmail.com>
2023-08-07 18:18:37 +0200

    Fixed byteSize and speed

--- Transmission/Transmission/TorrentsPage/Helpers/Int+Byte.swift
+++ Transmission/Transmission/TorrentsPage/Helpers/Int+Byte.swift
@@ -9,36 +9,30 @@ import Foundation
 
 extension Int {
 	var byteSize: String {
+		
+		let byte = 1024
+		let kByte = 1024 * byte
+		let mByte = 1024 * kByte
+		let gByte = 1024 * mByte
+		let tbyte = 1024 * gByte
+		
 		switch self {
-		case 0 ..< 1_000: // Byte
+		case 0 ..< byte: // Byte
 			return "\(self) B"
-		case 1_001 ..< 1_000_000: // Kbyte
-			return "\((Double(self) / Double(1_000)).round() ?? "-") KB"
-		case 1_000_001 ..< 1_000_000_000: // Mbyte
-			return "\((Double(self) / Double(1_000_000)).round() ?? "-") MB"
-		case 1_000_000_001 ..< 1_000_000_000_000: // Gbyte
-			return "\((Double(self) / Double(1_000_000_000)).round() ?? "-") GB"
-		case 1_000_000_000_001 ..< 1_000_000_000_000_000: // Tbyte
-			return "\((Double(self) / Double(1_000_000_000_000)).round() ?? "-") TB"
+		case byte + 1 ..< kByte: // Kbyte
+			return "\((Double(self) / Double(byte)).round() ?? "-") KB"
+		case kByte + 1 ..< mByte: // Mbyte
+			return "\((Double(self) / Double(kByte)).round() ?? "-") MB"
+		case mByte + 1 ..< gByte: // Gbyte
+			return "\((Double(self) / Double(mByte)).round() ?? "-") GB"
+		case gByte + 1 ..< tbyte: // Tbyte
+			return "\((Double(self) / Double(gByte)).round() ?? "-") TB"
 		default:
 			return "-"
 		}
 	}
 	
 	var speed: String {
-		switch self {
-		case 0 ..< 1_000: // Byte
-			return "\(self) B/s"
-		case 1_001 ..< 1_000_000: // Kbyte
-			return "\((Double(self) / Double(1_000)).round() ?? "-") KB/s"
-		case 1_000_001 ..< 1_000_000_000: // Mbyte
-			return "\((Double(self) / Double(1_000_000)).round() ?? "-") MB/s"
-		case 1_000_000_001 ..< 1_000_000_000_000: // Gbyte
-			return "\((Double(self) / Double(1_000_000_000)).round() ?? "-") GB/s"
-		case 1_000_000_000_001 ..< 1_000_000_000_000_000: // Tbyte
-			return "\((Double(self) / Double(1_000_000_000_000)).round() ?? "-") TB/s"
-		default:
-			return "-"
-		}
+		byteSize + "/s"
 	}
 }
