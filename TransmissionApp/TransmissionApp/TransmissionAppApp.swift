e99ec72d403244891bd330095d74f7b0aeaefaaa
Federico Arvat <arvat.federico@gmail.com>
2023-08-07 18:18:33 +0200

    Created workspace for TransmissionApp

---
+++ TransmissionApp/TransmissionApp/TransmissionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransmissionAppApp.swift
+//  TransmissionApp
+//
+//  Created by Federico Arvat on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TransmissionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
