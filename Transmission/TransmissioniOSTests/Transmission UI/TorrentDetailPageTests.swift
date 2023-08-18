6f0c04c3bd369ad4a534d9e09f8e15531f7c88ef
Federico Arvat <arvat.federico@gmail.com>
2023-08-18 16:29:23 +0200

    Added snapshot test for TorrentDetailPage

---
+++ Transmission/TransmissioniOSTests/Transmission UI/TorrentDetailPageTests.swift
@@ -0,0 +1,43 @@
+//
+//  TorrentDetailPageTests.swift
+//  TransmissioniOSTests
+//
+//  Created by Federico Arvat on 18/08/23.
+//
+
+import XCTest
+@testable import Transmission
+@testable import TransmissioniOS
+
+class TorrentDetailPageTests: XCTestCase {
+	func test_torrentDetailPage() {
+		let sut = makeSUT()
+		
+		assert(snapshot: sut.snapshot(.iPhone13(style: .light)), named: "TORRENT_DETAIL_PAGE_light")
+		assert(snapshot: sut.snapshot(.iPhone13(style: .dark)), named: "TORRENT_DETAIL_PAGE_dark")
+		assert(snapshot: sut.snapshot(.iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "TORRENT_DETAIL_PAGE_light_extraExtraExtraLarge")
+	}
+	
+	// MARK: - Helpers
+	
+	private func makeSUT() -> TorrentDetailPage {
+		TorrentDetailPage(
+			viewModel: TorrentDetailPageViewModel(
+				name: "Torrent name",
+				percentageCompleted: "56,98%",
+				uploaded: "123,00 MB",
+				ratio: "0,09",
+				downloaded: "234,98 MB",
+				state: "Downloading",
+				runningTime: "24 hours",
+				remainingTime: "2 hours",
+				lastActivity: "12 hours ago",
+				error: "None",
+				size: "123,00 GB",
+				location: "/here",
+				hash: "ex2m190ixs9021ixs90m1i2s",
+				privacy: "Public torrent"
+			)
+		)
+	}
+}
