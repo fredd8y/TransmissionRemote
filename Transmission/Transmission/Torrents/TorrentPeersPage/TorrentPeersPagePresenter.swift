7368871799d634a4312e7bb3be69a7d229cccc17
Federico Arvat <arvat.federico@gmail.com>
2023-08-20 21:55:50 +0200

    Asserted that TorrentPeersPagePresenter map correctly view models

--- Transmission/Transmission/Torrents/TorrentPeersPage/TorrentPeersPagePresenter.swift
+++ Transmission/Transmission/Torrents/TorrentPeersPage/TorrentPeersPagePresenter.swift
@@ -17,4 +17,16 @@ public class TorrentPeersPagePresenter {
 			comment: "Page title")
 	}
 	
+	public static func map(_ torrentPeers: [TorrentPeer]) -> TorrentPeersPageViewModel {
+		TorrentPeersPageViewModel(
+			peers: torrentPeers.map {
+				TorrentPeersPageViewModel.Peer(
+					percentage: (($0.progress * 100).round() ?? "-") + "%",
+					status: $0.status,
+					address: $0.address,
+					client: $0.clientName
+				)
+			}
+		)
+	}
 }
--- Transmission/TransmissionTests/Torrents/TorrentPeersPage/TorrentPeersPagePresenterTests.swift
+++ Transmission/TransmissionTests/Torrents/TorrentPeersPage/TorrentPeersPagePresenterTests.swift
@@ -16,4 +16,25 @@ final class TorrentPeersPagePresenterTests: XCTorrentTestCase {
 		XCTAssertEqual(TorrentPeersPagePresenter.title, localized("TITLE", table: table))
 	}
 	
+	func test_map_createsViewModel() {
+		let progress = 0.97
+		let status = "Status1"
+		let address = "Address1"
+		let clientName = "Client1"
+		
+		let viewModel = TorrentPeersPagePresenter.map([
+			TorrentPeer(
+				progress: progress,
+				status: status,
+				address: address,
+				clientName: clientName
+			)
+		])
+		
+		XCTAssertEqual(viewModel.peers.first?.percentage, "97,00%")
+		XCTAssertEqual(viewModel.peers.first?.status, status)
+		XCTAssertEqual(viewModel.peers.first?.address, address)
+		XCTAssertEqual(viewModel.peers.first?.client, clientName)
+	}
+	
 }
