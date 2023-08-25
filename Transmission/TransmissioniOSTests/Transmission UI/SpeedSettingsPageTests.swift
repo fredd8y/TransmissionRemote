819db4d74fb93e2dbe7d57f2d9631d358cb9a094
Federico Arvat <arvat.federico@gmail.com>
2023-08-25 20:33:31 +0200

    Asserted that speed settings page with all toggle off has correct layout

--- Transmission/TransmissioniOSTests/Transmission UI/SpeedSettingsPageTests.swift
+++ Transmission/TransmissioniOSTests/Transmission UI/SpeedSettingsPageTests.swift
@@ -19,9 +19,36 @@ class SpeedSettingsPageTests: XCTestCase {
 		assert(snapshot: sut.snapshot(.iPhone13(style: .dark)), named: "SPEED_SETTINGS_PAGE_ALL_TOGGLE_ON_dark")
 		assert(snapshot: sut.snapshot(.iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "SPEED_SETTINGS_PAGE_ALL_TOGGLE_ON_light_extraExtraExtraLarge")
 	}
+	func test_settingsPageWithAllToggleOff() {
+		let sut = makeSUTWithAllToggleOff()
+		
+		assert(snapshot: sut.snapshot(.iPhone13(style: .light)), named: "SPEED_SETTINGS_PAGE_ALL_TOGGLE_OFF_light")
+		assert(snapshot: sut.snapshot(.iPhone13(style: .dark)), named: "SPEED_SETTINGS_PAGE_ALL_TOGGLE_OFF_dark")
+		assert(snapshot: sut.snapshot(.iPhone13(style: .light, contentSize: .extraExtraExtraLarge)), named: "SPEED_SETTINGS_PAGE_ALL_TOGGLE_OFF_light_extraExtraExtraLarge")
+	}
 	
 	// MARK: - Helpers
 	
+	private func makeSUTWithAllToggleOff() -> SpeedSettingsPage {
+		SpeedSettingsPage(viewModel: SpeedSettingsPageViewModel(
+			isLoading: false,
+			errorMessage: nil,
+			uploadLimitEnabled: false,
+			uploadLimit: "100",
+			uploadLimitError: false,
+			downloadLimitEnabled: false,
+			downloadLimit: "100",
+			downloadLimitError: false,
+			alternativeUploadLimit: "100",
+			alternativeUploadLimitError: false,
+			alternativeDownloadLimit: "100",
+			alternativeDownloadLimitError: false,
+			scheduledTimesEnabled: false,
+			alternativeSpeedTimeBegin: .hour0000,
+			alternativeSpeedTimeDay: .sunday,
+			alternativeSpeedTimeEnd: .hour0000
+		))
+	}
 	private func makeSUTWithAllToggleOn() -> SpeedSettingsPage {
 		SpeedSettingsPage(viewModel: SpeedSettingsPageViewModel(
 			isLoading: false,
