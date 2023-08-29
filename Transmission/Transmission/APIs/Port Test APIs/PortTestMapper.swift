69976ba257629be3e4bd7073a6947a9dc4c1a954
Federico Arvat <arvat.federico@gmail.com>
2023-08-29 18:21:35 +0200

    Added port test mapper with tests

--- Transmission/Transmission/APIs/Port Test APIs/PortTestMapper.swift
+++ Transmission/Transmission/APIs/Port Test APIs/PortTestMapper.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+public enum PortTestMapper {
+	
+	public enum Error: Swift.Error {
+		case invalidData
+	}
+	
+	public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Bool {
+		guard response.isOK, let remoteResponse = try? JSONDecoder().decode(RemoteResponse.self, from: data) else {
+			throw Error.invalidData
+		}
+		return remoteResponse.arguments.portIsOpen
+	}
+	
+	private struct RemoteResponse: Decodable {
+		let arguments: Arguments
+		let result: String
+		
+		struct Arguments: Decodable {
+			let portIsOpen: Bool
+			
+			enum CodingKeys: String, CodingKey {
+				case portIsOpen = "port-is-open"
+			}
+		}
+	}
+	
+}
--- Transmission/TransmissionTests/APIs/Port Test APIs/PortTestMapperTests.swift
+++ Transmission/TransmissionTests/APIs/Port Test APIs/PortTestMapperTests.swift
@@ -5,4 +5,74 @@
 //  Created by Federico Arvat on 29/08/23.
 //
 
-import Foundation
+import XCTest
+import Transmission
+
+class PortTestMapperTests: XCTestCase {
+	
+	func test_map_throwsErrorOnNon200HTTPResponse() throws {
+		let json = makeJSON(fromDictionary: [:])
+		let samples = [199, 201, 300, 408, 500]
+		
+		try samples.forEach { code in
+			XCTAssertThrowsError(
+				try PortTestMapper.map(json, from: HTTPURLResponse(statusCode: code))
+			)
+		}
+	}
+	
+	func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() {
+		let json = Data("invalid json".utf8)
+		
+		XCTAssertThrowsError(
+			try PortTestMapper.map(json, from: HTTPURLResponse(statusCode: 200))
+		)
+	}
+	
+	func test_map_throwsErrorOn200HTTPResponseWithEmptyJSON() throws {
+		XCTAssertThrowsError(
+			try PortTestMapper.map(Data(), from: HTTPURLResponse(statusCode: 200))
+		)
+	}
+	
+	func test_map_deliversOn200HTTPResponseWithValidJSON_open() {
+		let portTest = makePortTestOpen(true)
+		
+		let json = makeJSON(fromDictionary: portTest.json)
+		
+		do {
+			let result = try PortTestMapper.map(json, from: HTTPURLResponse(statusCode: 200))
+			
+			XCTAssertEqual(result, portTest.portIsOpen)
+		} catch {
+			XCTFail("Expected no exception, got \(error.localizedDescription) instead")
+		}
+	}
+	
+	func test_map_deliversOn200HTTPResponseWithValidJSON_closed() {
+		let portTest = makePortTestOpen(false)
+		
+		let json = makeJSON(fromDictionary: portTest.json)
+		
+		do {
+			let result = try PortTestMapper.map(json, from: HTTPURLResponse(statusCode: 200))
+			
+			XCTAssertEqual(result, portTest.portIsOpen)
+		} catch {
+			XCTFail("Expected no exception, got \(error.localizedDescription) instead")
+		}
+	}
+	
+	// MARK: - Helpers
+	
+	private func makePortTestOpen(_ open: Bool) -> (json: [String: Any], portIsOpen: Bool) {
+		let json: [String: Any] = [
+			"result": "success",
+			"arguments": [
+				"port-is-open": open
+			]
+		]
+		return (json, open)
+	}
+	
+}
